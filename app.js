41c3bfb4597ac1cfe906a8b0a7dec1fb067cc317
tiago staurenghi <tiagostaurenghi@gmail.com>
2019-10-10 09:06:17 -0300

    correçoes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-var app = angular.module("cookie-clicker", []);
+var app = angular.module("CookieClicker", []);
 
-app.controller("cookie-controller", function($scope)){
+app.controller("cookie-controller", function($scope){
     $scope.cookies = 0;
     $scope.click = function() {
         $scope.cookies++;
+        console.log($scope.cookies);
     }
-};
+
+});
